51912b61e7f26422612b552bbf6b57ec36a91587
KAMIT\KamkIGCC <shishanth.kaliannan@gmail.com>
2018-09-04 11:27:05 +0300

    Added rotating sun and background

---
+++ RhythmAnalyzer/Assets/ShishScripts/SunSpin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSpin : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetChild(0).transform.Rotate(new Vector3(0, 0, speed));
+        transform.GetChild(1).transform.Rotate(new Vector3(0, 0, -speed));
+    }
+}
